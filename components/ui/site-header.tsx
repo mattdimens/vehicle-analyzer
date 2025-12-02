e9388d7e5e5f31c4d44ff9431650bbf9d4841f5b
Matt DiMenno <mattdimens@gmail.com>
2025-12-01 22:06:21 -0500

    mobile nav updates

--- components/ui/site-header.tsx
+++ components/ui/site-header.tsx
@@ -60,7 +60,7 @@ export function SiteHeader() {
               <Menu className="h-6 w-6" />
             </Button>
           </SheetTrigger>
-          <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+          <SheetContent side="right" className="w-[300px] sm:w-[400px] p-6">
             <nav className="flex flex-col gap-4 mt-8">
               <Link
                 href="#how-it-works"
